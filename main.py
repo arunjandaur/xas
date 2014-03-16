1bccbb380e3667ca7eec36d851bb1497e1ac8a2e
arunjandaur <arun.jandaur@gmail.com>
2014-03-15 22:59:19 -0700

    Edited the main.py and isolated intensity parser. Also worked on some regex stuff so xas parsing is more accurate. those changes are in main. They are not done yet

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import sys
 import numpy as np
 import mdp
-import re
 import os
 
 from CalcDistanceNode import CalcDistanceNode
 from PermutationNode import PermutationNode
+from parse_intensities import parse_intensities
 
-XAS_FOLDER_NAME = "xas"
 SNAPSHOTS_FOLDER_NAME = "snapshots"
 OUTPUT_FOLDER = "dist_and_intens"
 NUM_ENERGIES = 1000
@@ -18,35 +17,6 @@ lattice_c = 5
 alpha = 90.0
 beta = 90.0
 gamma = 90.0
-
-def parse_intensities(atomName, atomNum, xyz_name):
-	"""
-	Takes the name of an xyz snapshot, an atom name (label), and which line the atom is found on, and then constructs the file name of the corresponding xas file. It then takes the 1000 energy intensity key value pairs in that xas file and converts them into a dictionary, which exists inside of a 2D numpy array. 
-	Invariant: XYZ file must end in .xyz. Otherwise, xyzWithSnapNum = xyz_name[0:len(xyz_name)-4] will fail. It depends on the last four characters to be .xyz
-	"""
-	"""
-	if atomNum < 10:
-		atomNum = '00' + str(atomNum)
-	elif atomNum < 100:
-		atomNum = '0' + str(atomNum)
-	else:
-		atomNum = str(atomNum)
-	"""
-	xyzWithSnapNum = xyz_name[0:len(xyz_name)-4]
-	xyzWithoutSnapNum = re.sub(r'(.*)_[0-9]+[.]xyz', r'\1', xyz_name)
-	xas_filename = XAS_FOLDER_NAME + '/' + xyzWithSnapNum + '-' + xyzWithoutSnapNum + '.' + atomName + str(atomNum) + '-XCH.xas.5.xas'
-	
-	intensities = np.array([[{}]])
-	if os.path.isfile(xas_filename) == True:
-		intensities[0][0] = 2
-		#raw_xas = np.loadtxt(xas_filename, usecols=(0, 1))
-		#for row in raw_xas:
-			#intensities[0][0][row[0]] = row[1]
-	else:
-		intensities[0][0] = -1
-
-	return intensities
-
 
 if __name__ == '__main__':
 	atomLabels = np.empty((0, 1))
@@ -94,5 +64,5 @@ if __name__ == '__main__':
 			intenFile = open(intenFileName, 'w')
 			intenFile.write(str(intenArray))
 	
-	np.set_printoptions(threshold='nan', precision=3)
+	np.set_printoptions(threshold='nan', precision=4)
 	print permutedDistanceArray
---
+++ parse_intensities.py
@@ -0,0 +1,32 @@
+import re
+import numpy as np
+import os
+
+XAS_FOLDER_NAME = "xas"
+
+def parse_intensities(atomName, atomNum, xyz_name):
+        """
+        Takes the name of an xyz snapshot, an atom name (label), and which line the atom is found on, and then constructs the file name of the corresponding xas file. It then takes the 1000 energy intensity key value pairs in that xas file and converts them into a dictionary, which exists inside of a 2D numpy array. 
+        Invariant: XYZ file must end in .xyz. Otherwise, xyzWithSnapNum = xyz_name[0:len(xyz_name)-4] will fail. It depends on the last four characters to be .xyz
+        """
+        """
+        if atomNum < 10:
+                atomNum = '00' + str(atomNum)
+        elif atomNum < 100:
+                atomNum = '0' + str(atomNum)
+        else:
+                atomNum = str(atomNum)
+        """
+        xyzWithSnapNum = xyz_name[0:len(xyz_name)-4]
+        xyzWithoutSnapNum = re.sub(r'(.*)_[0-9]+[.]xyz', r'\1', xyz_name)
+        xas_filename = XAS_FOLDER_NAME + '/' + xyzWithSnapNum + '-' + xyzWithoutSnapNum + '.' + atomName + str(atomNum) + '-XCH.xas.5.xas'
+        
+        intensities = np.array([[{}]])
+        if os.path.isfile(xas_filename) == True:
+                raw_xas = np.loadtxt(xas_filename, usecols=(0, 1))
+                for row in raw_xas:
+                        intensities[0][0][row[0]] = row[1]
+        else:
+                intensities[0][0] = -1
+
+        return intensities
