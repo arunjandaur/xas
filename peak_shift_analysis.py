34d772ebc30456c497b502acbf5e2ed7c7432e79
arunjandaur <arun.jandaur@gmail.com>
2014-06-23 16:33:15 -0700

    Implemented gaussian mean functional dependence regression for one gaussian peak

--- peak_shift_analysis.py
+++ peak_shift_analysis.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 from numpy import random
+import matplotlib.pyplot as plt
 from sklearn.cluster import *
 from sklearn.mixture import GMM
 from math import *
@@ -24,20 +25,29 @@ def peak_crossing():
 def peak_split():
 	pass
 
-def single_peak_variable_fit():
-	mean = lambda x: x
-	gauss = lambda E, x: 1 / sqrt(2*pi) * np.exp(-.5 * np.power(E - mean(x), 2))
-
-	x = 50
-	interval = [mean(x)-2.0, mean(x)+2.0]
-	#energies = np.linspace(interval[0], interval[1], 1000)
-	dist = norm(loc=mean(x), scale=1)
-	energies = dist.rvs(size=1000)
-	print energies
-	intens = gauss(energies, x)
-	return norm.fit(energies)[0]
-	#return curve_fit(gauss, energies, intens, p0=9)[0]
-	#return norm.fit(np.transpose(np.vstack((energies, intens))), loc=500)[0]
+def gauss(E, sigma, a):
+	x = E[:, 0]
+	energy = E[:, 1]
+	A = 1 / (sigma * sqrt(2*pi))
+	return A * np.exp(-.5 * np.power((energy - a*x) / sigma, 2))
 
 if __name__ == "__main__":
-	print single_peak_variable_fit()
+	X = np.array([100, 120, 140, 160, 180, 200, 210, 215, 500])
+	sigma = 50
+	a = 3
+	E = np.array([[], []])
+
+	for x in X:
+		mean = a*x
+		xdata = np.linspace(mean-200, mean+200, 10000)
+		x_s = [x for _ in range(len(xdata))]
+		temp = np.vstack((x_s, xdata))
+		E = np.hstack((E, temp))
+
+	E = np.transpose(E)
+	I = gauss(E, sigma, a)
+
+	fitparams, fitcovariance = curve_fit(gauss, E, I, p0 = [5000, 0])
+	plt.plot(E, I, label = 'original data')
+	plt.plot(E, gauss(E, *fitparams) ,'bo',label = "fit curve")
+	plt.legend()
