ed260b691f47325ada6f59101eb657aa72a3da53
arunjandaur <arun.jandaur@gmail.com>
2014-06-26 16:08:39 -0700

    stuff

--- peak_shift_analysis.py
+++ peak_shift_analysis.py
@@ -32,33 +32,44 @@ def gauss(E, sigma, a, b, a2):
 	A = 1 / (sigma * sqrt(2*pi))
 	return A * np.exp(-.5 * np.power((energy - (a*x+b+a2*x2)) / sigma, 2))
 
+def gauss2(E, sigma1, sigma2, a1, b1, c1, a2, b2, c2):
+	return gauss(E, sigma1, a1, b1, c1) + gauss(E, sigma2, a2, b2, c2)
+
 if __name__ == "__main__":
-	X = np.array([1, 2, 3, 4, 5])
-	X2 = np.array([1, 1.4, 2.8, 3.2, 4.5])
-	sigma = .5
-	a = 3
-	b = 3
-	a2 = 0
+	X = (np.random.normal(loc=1.16, scale=.16, size=10) - 1.16) / .16
+	X2 = (np.random.normal(loc=3, scale=.3, size=10) - 3) / .3
+	sigma1 = .5
+	sigma2 = .55
+	a1 = .8
+	b1 = 0
+	c1 = 1
+	a2 = -.8
+	b2 = 0
+	c2 = 6
 	E = np.array([[], [], []])
 
 	for i in range(len(X)):
 		x = X[i]
 		x2 = X2[i]
-		mean = a*x + b + a2*x2
-		xdata = np.linspace(mean-2.5*sigma, mean+2.5*sigma, 1000)
+		mean = a1*x + b1*x2 + c1
+		mean2 = a2*x + b2*x2 + c2
+		#interv1 = np.linspace(mean-2.5*sigma1, mean+2.5*sigma1, 10000)
+		#interv2 = np.linspace(mean2-2.5*sigma2, mean2+2.5*sigma2, 10000)
+		#xdata = np.hstack((interv1, interv2))
+		xdata = np.linspace(mean-2.5*sigma1, mean2+2.5*sigma2)
 		x_s = [x for _ in range(len(xdata))]
 		x2_s = [x2 for _ in range(len(xdata))]
 		temp = np.vstack((x_s, x2_s, xdata))
 		E = np.hstack((E, temp))
 
 	E = np.transpose(E)
-	I = gauss(E, sigma, a, b, a2)
-	noise = random.random(len(I)) * max(I)*.05
+	I = gauss2(E, sigma1, sigma2, a1, b1, c1, a2, b2, c2)
+	noise = random.random(len(I)) * np.mean(I)*.025
 	noisyI = I + noise
-	noise = np.transpose(np.vstack((np.zeros(len(E)), np.zeros(len(E)), random.random(len(E)) * .05*max(X))))
+	noise = np.transpose(np.vstack((np.zeros(len(E)), np.zeros(len(E)), random.random(len(E)) * .025*np.mean(E[:,2]))))
 	noisyE = E + noise
 
-	fitparams, fitcovariance = curve_fit(gauss, noisyE, noisyI, p0 = [2.5, 1, 0, 4], maxfev=4000)
-	plt.plot(noisyE[:, 0], noisyI, label = 'original data')
-	plt.plot(E[:, 0], gauss(E, *fitparams) ,'bo',label = "fit curve")
+	fitparams, fitcovariance = curve_fit(gauss2, noisyE, noisyI, p0 = [50, 50, 1, 1, 1, -1, 1, 4], maxfev=4000)
+	plt.plot(noisyE[:,2], noisyI, 'ro', label = 'original data')
+	plt.plot(E[:,2], gauss2(E, *fitparams), 'bo', label = "fit curve")
 	plt.legend()
