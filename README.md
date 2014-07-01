07abba8189c91cda3938a104f578100eb3b4dce3
arunjandaur <arun.jandaur@gmail.com>
2014-06-30 17:37:11 -0700

    Worked on number of gaussian detection

--- README.md
+++
@@ -1,6 +0,0 @@
-Attention: This project is in the prototype phase. Assume nothing works yet.
-
-Process: xyz file ==> LatticeTransform & DistanceNode ==> PermutationNode
-
-main.py oversees the process.
-main executes main.py
--- peak_shift_analysis.py
+++ peak_shift_analysis.py
@@ -2,28 +2,9 @@ from __future__ import division
 import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
-from sklearn.cluster import *
-from sklearn.mixture import GMM
 from math import *
 from scipy.optimize import curve_fit
-from scipy.stats import norm
-
-def peak_crossing():
-	x1 = np.transpose(np.array([list(np.arange(1, 101, 1))]))
-	#x2 = np.array([list(np.arange(8, 13, 1))])
-	y1 = x1
-	z1 = np.transpose(np.array([list(random.random(100))])) * 10.0
-	y2 = np.transpose(np.array([list(random.random(100))])) * 10.0
-	z2 = -1 * x1
-
-	X = np.hstack((np.vstack((x1, x1)), np.vstack((y1, y2)), np.vstack((z1, z2))))
-
-	g = GMM(n_components=2)
-	g.fit(X)
-	print g.labels_
-
-def peak_split():
-	pass
+from scipy.ndimage.filters import gaussian_filter1d
 
 def gauss(E, sigma, a, b, c):
 	x = E[:, 0]
@@ -35,7 +16,21 @@ def gauss(E, sigma, a, b, c):
 def gauss2(E, sigma1, sigma2, a1, b1, c1, a2, b2, c2):
 	return gauss(E, sigma1, a1, b1, c1) + gauss(E, sigma2, a2, b2, c2)
 
+def gauss_simple(E, A, avg, sigma):
+	return A * np.exp(-.5 * np.power((E-avg) / sigma, 2))
+
+def num_gaussians(data):
+	newdata = gaussian_filter1d(data, sigma=.1, mode='constant')
+	return newdata
+
 if __name__ == "__main__":
+	E = np.linspace(0, 8, 100)
+	I = gauss_simple(E, 8, 5, 1) + gauss_simple(E, 8, 6, 1)
+	newI = num_gaussians(I)
+	plt.plot(E, newI, 'ro', label='fit')
+	plt.plot(E, I, 'b', label='original')
+	plt.show()
+	"""	
 	X = (np.random.normal(loc=1.16, scale=.16, size=10) - 1.16) / .16
 	X2 = (np.random.normal(loc=3, scale=.3, size=10) - 3) / .3
 	sigma1 = .5
@@ -53,9 +48,6 @@ if __name__ == "__main__":
 		x2 = X2[i]
 		mean = a1*x + b1*x2 + c1
 		mean2 = a2*x + b2*x2 + c2
-		#interv1 = np.linspace(mean-2.5*sigma1, mean+2.5*sigma1, 10000)
-		#interv2 = np.linspace(mean2-2.5*sigma2, mean2+2.5*sigma2, 10000)
-		#xdata = np.hstack((interv1, interv2))
 		xdata = np.linspace(mean-2.5*sigma1, mean2+2.5*sigma2, 1000)
 		x_s = [x for _ in range(len(xdata))]
 		x2_s = [x2 for _ in range(len(xdata))]
@@ -73,3 +65,4 @@ if __name__ == "__main__":
 	plt.plot(noisyE[:,2], noisyI, 'ro', label = 'original data')
 	plt.plot(E[:,2], gauss2(E, *fitparams), 'bo', label = "fit curve")
 	plt.legend()
+	"""
