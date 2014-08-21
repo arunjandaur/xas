Fixed, improved, and tested distance calculation

CalcDistanceNode.py
@@ -3,11 +3,13 @@ from numpy import *
 from math import sqrt
 from LatticeTransformNode import *
 
-def coordsToDistance(x1, y1, z1, x2, y2, z2):
+def _coordsToDistance(coord1, coord2):
     """
     Input: 6 floats that represent the xyz coordinates of atom 1 and atom 2
     Output: 1 float that represents the euclidean distance between atom 1 and atom 2
     """
+    x1, y1, z1 = coord1
+    x2, y2, z2 = coord2
     return sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
 
 class CalcDistanceNode(mdp.Node):
@@ -30,28 +32,15 @@ class CalcDistanceNode(mdp.Node):
         if len(args) != 0:
             lattice_a, lattice_b, lattice_c, alpha, beta, gamma = args
             latticeNode = LatticeTransformNode(lattice_a, lattice_b, lattice_c, alpha, beta, gamma)
-            i = 0
-            for atomCoord1 in coords:
-                j = 0
-                centeredCoords = latticeNode(array([atomCoord1]), coords) #Center the coordinates around atomCoord1
-                center_coord = centeredCoords[0] #The centered coordinate
-                centeredCoordsArray = centeredCoords[1] #The coordinates surrounding center_coord
-                x1 = center_coord[0][0] #Get x, y, and z
-                y1 = center_coord[0][1]
-                z1 = center_coord[0][2]
-                for atomCoord2 in centeredCoordsArray: #for each element j in J, compute dist(i, j)
-                    x2 = atomCoord2[0]#Get x, y, and z
-                    y2 = atomCoord2[1]
-                    z2 = atomCoord2[2]
-                    dists[i][j] = coordsToDistance(x1, y1, z1, x2, y2, z2)
-                    j = j + 1
-                i = i + 1
 
+            for i in range(len(coords)):
+                center, neighbors = latticeNode(np.array([coords[i]]), np.vstack((coords[0:i], coords[i+1])))
+                coords2 = np.vstack((neighbors[0:i], center, neighbors[i:]))
+                for j in range(len(coords2)):
+                    dists[i][j] = _coordsToDistance(center[0], coords[j])
             return dists
 
         for i in range(len(coords)):
-            x1, y1, z1 = coords[i]
             for j in range(len(coords)):
-                x2, y2, z2 = coords[j]
-                dists[i][j] = coordsToDistance(x1, y1, z1, x2, y2, z2)
+                dists[i][j] = _coordsToDistance(coords[i], coords[j])
         return dists